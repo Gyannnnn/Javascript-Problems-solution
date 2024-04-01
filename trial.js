9bebd4971b672a40361b79f0dfd672d02f768010
Gyanranjan Patra <hi.gyanaranjanpatra@gmail.com>
2024-04-01 22:11:42 +0530

    5

---
+++ trial.js
@@ -0,0 +1,65 @@
+// console.log("Loops");
+
+// const  obj = {
+//     Name:"Gyanranjan Patra",
+//     Branch:"Information Technology",
+//     Rollno:"2302080019"
+// }
+
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//         console.log(obj)
+//     }
+// }
+
+
+
+
+// functions
+
+// function sum(a,b){
+//     console.log(a+b);
+//     return a+b;
+// }
+
+// var g = sum(3,4);
+// console.log(g);
+// sum(1,2)
+
+
+// <-----------------------------:STRING:-------------------------------------------->
+
+
+
+// let name1 = "Gyanranjan Patra"
+// console.log(name1)
+// var g = name1.length
+// console.log(g)
+// name1.toLocaleLowerCase
+// console.log(name1)
+
+
+let name1 = "Gyanranjan Patra"
+let newname = name1.toUpperCase()
+console.log(newname);
+let newname1 = name1.toLowerCase()
+console.log(newname1);
+let sl = name1.slice(3,9)
+console.log(sl);
+let s2 = name1.slice(3);
+console.log(s2);
+let r = name1.replace("Patra","xxxxxxx")
+console.log(r);
+
+
+let c1 = "Gyana"
+let c2 = "Runa"
+
+let concate = c1+c2;
+console.log(concate)
+let t = "       gyanranjanrunababu       "
+let t1 = t.trim();
+console.log(t1);
+
+console.log(name1[15])
